2c1d8a6737f7c7af19ec2a930c420b209422f9fd
castengo <catalina.astengo@hotmail.com>
2016-10-07 16:00:54 -0700

    Fix category URL

--- mod4-solution/src/data/categories.component.js
+++ mod4-solution/src/data/categories.component.js
@@ -2,7 +2,7 @@
 
 angular.module('Data')
 .component('categories', {
-  templateUrl: "/src/templates/category.html",
+  templateUrl: "src/templates/category.html",
   bindings: {
     categories: '<list'
   }
